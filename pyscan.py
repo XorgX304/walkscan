de1dd1cbb9f38eea0d1d84a703f89398742338ac
mr0mx <mr0mx@localhost.localdomain>
2017-05-07 14:36:16 -0300

    update 2.0

--- pyscan.py
+++ pyscan.py
@@ -1,32 +1,63 @@
 import socket
+import sys
+import os
+from sys import platform
+from os import system
 
-print('''\033[0;35m
-###############################################
-#[+]Author:   mr0mx                           #
-#[+]Email:    devgabrieldutra@gmail.com       #
-#[+]Github:   github.com/mr0mx                #
-#[+]Facebook: fb.com/mr0mx                    #
-#[+]Version:  1.0                             #
-#[+]Exemple:  Host: 192.168.1.1               #
-#             TimeOut: 0.05                   #
-###############################################
-\033[0;36m
-''')
+def sistema():
 
-host = raw_input("Host: ")
-timeout = float(raw_input("Timeout: <default = 0.05>: "))
+    if platform.startswith('linux'):
+        system("clear")
+    else:
+        system("cls")
 
-ports = [21, 22, 23, 25, 80, 135, 443, 3306]
 
-for port in ports:
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client.settimeout(timeout)
-	resp = client.connect_ex((host, port))
+def banner():
+    print('''
 
-	if resp == 0:
-		print str(port) + "-> Open"
+ _______           _______  _______  _______  _       
+(  ____ )|\     /|(  ____ \(  ____ \(  ___  )( (    /|
+| (    )|( \   / )| (    \/| (    \/| (   ) ||  \  ( |
+| (____)| \ (_) / | (_____ | |      | (___) ||   \ | |
+|  _____)  \   /  (_____  )| |      |  ___  || (\ \) |
+| (         ) (         ) || |      | (   ) || | \   |
+| )         | |   /\____) || (____/\| )   ( || )  \  |
+|/          \_/   \_______)(_______/|/     \||/    )_)
 
-	else:
-		print str(port) + "-> Closed"
+                                                      
+github: github.com/gDutr4
+email: devgabrieldutra@gmail.com
 
-print "Scan Finalizado"
+    ''')
+
+'''
+def argumentos():
+
+    if len(sys.argv) != 2:
+        print "#=========================================#"
+        print "#  Exemplo: python pyscan.py google.com   #"
+        print "#=========================================#"
+'''
+
+def main():
+    host = sys.argv[1];
+
+    ports = [21, 22, 23, 25, 26, 53, 57, 80, 81, 88,
+            115, 118, 119, 137, 138, 135, 443, 445,
+            514, 901, 911, 1521, 2082, 3306, 5435]
+
+    for port in ports:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.05)
+        r = client.connect_ex((host, port))
+
+        if r == 0:
+            print str(port) + "-> open"
+
+sistema()
+banner()
+main()
+
+print "#======================#"
+print "#     Finish scan      #"
+print "#======================#"
