d7954177e52d6ba501363a7ccabe190c48a585da
root <root@localhost.localdomain>
2017-07-30 17:40:34 -0300

    News features

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+**pyscan** A command line tool to port scan!
+===================
+**usage:**
 
+usage: pyscan.py [-h] [--host HOST] [-w File]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --host HOST           Informe o host
+  -w File, --write File
+                        Save the output results.
--- pyscan.py
+++ pyscan.py
@@ -1,20 +1,21 @@
+#!/usr/bin/python3
 import socket
 import sys
 import argparse
 from sys import platform
 import os
-
+import time
 
 def start_system():
 
     if platform.startswith('linux') or platform.startswith('darwin'):
-       os.system("clear")
-       return
+        os.system("clear")
+        return
     else:
         os.system("cls")
     
 def all_services():
-	return {
+    return {
 		21: "FTP",
 		22: "SSH",
 		23: "TelNet",
@@ -62,62 +63,71 @@ email: devgabrieldutra@gmail.com
     ''')
 
 def attempt_connections(host):
-	"""Attempts to connect to the ports pre defined in the all_services function
-	:param host the connection target host
-	"""
-	open_ports = []
 
-	for port in all_services().keys():
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		client.settimeout(0.05)
-                host = socket.gethostbyname(host)
-		if client.connect_ex((host, port)) == 0:
-			open_ports.append(port)
-                client.close
-	return open_ports, host
+    """Attempts to connect to the ports pre defined in the all_services function
+    :param host the connection target host
+    """
+
+    open_ports =[]
+    t1=time.time()
+    for port in all_services().keys():
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.05)
+        host = socket.gethostbyname(host)
+        if client.connect_ex((host, port)) == 0:
+            open_ports.append(port)
+            client.close
+    t2 = time.time()
+    total_time = "\nScanned in {:.2f} seconds".format(t2-t1)
+
+    return open_ports, host, total_time
 
 
 def print_results(host, open_ports):
-	"""Prints the results the results based on the open ports
-	:param host: the connection target host
-	:param open_ports: a list containing the open ports(ports must be int)
-	"""
-	#print("Target MOTHERFUCKING host is {}".format(host))
+
+    """Prints the results the results based on the open ports
+    :param host: the connection target host
+    :param open_ports: a list containing the open ports(ports must be int)
+    """
 	
-	print("------------------------------------------------------------")
-	print("Address: {}".format(attempt_connections(host)[1]))
-	print("Host: {}".format(host))
-	print("------------------------------------------------------------\n\n")
+    print("------------------------------------------------------------")
+    print("Address: {}".format(attempt_connections(host)[1]))
+    print("Host: {}".format(host))
+    print("------------------------------------------------------------\n\n")
 
-	if len(open_ports) == 0:
-		print("Nothing OPEN")
+    if len(open_ports) == 0:
+        print("Nothing OPEN")
 
-	else:
-		for port in open_ports:
-			print("{}\t({})\tis OPEN".format(port, all_services()[port]))
+    else:
+        print("Port:\tService:  Status:")
+        for port in open_ports:
+            print("{}\t{}\t  OPEN".format(port, all_services()[port]))
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--host', action='store', dest='host', help='Informe o host')
-        parser.add_argument('-w', '--write', help='Save the output results.', action='store', metavar='File')
-        args = parser.parse_args()
 
-        if args.write is not None:
-            sys.stdout = open(args.write, 'w')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', action='store', dest='host', help='Informe o host')
+    parser.add_argument('-w', '--write', help='Save the output results.', action='store', metavar='File')
+    args = parser.parse_args()
 
-        if args.host:                 
-            try:
-                host = args.host
-	        open_ports = attempt_connections(host)[0]
-	        print_results(host, open_ports)
-            except socket.gaierror:
-                print("You need to give proper hostname")
-        else:
-            parser.print_help()
-            sys.exit(1)
+    if args.write is not None:
+        print("Wait...\nWriting in {}".format(args.write))
+        sys.stdout = open(args.write, 'w')
+
+    if args.host:
+        try:
+            host = args.host
+            open_ports = attempt_connections(host)[0]
+            print_results(host, open_ports)
+            print(attempt_connections(host)[2])
+        except socket.gaierror:
+            print("You need to give proper hostname")
+    else:
+        parser.print_help()
+        sys.exit(1)
         
 
 if __name__ == '__main__':
-	start_system()
-	banner()
-	main()
+    start_system()
+    banner()
+    main()
